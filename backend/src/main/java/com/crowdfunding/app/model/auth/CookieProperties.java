ce25196e39ace67760d591052afc36c267af516c
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-09 13:33:31 -0800

    feature: adde cookie properties.

---
+++ backend/src/main/java/com/crowdfunding/app/model/auth/CookieProperties.java
@@ -0,0 +1,12 @@
+package com.crowdfunding.app.model.auth;
+
+import lombok.Data;
+
+@Data
+public class CookieProperties {
+	String domain;
+	String path;
+	boolean httpOnly;
+	boolean secure;
+	int maxAgeInMinutes;
+}
