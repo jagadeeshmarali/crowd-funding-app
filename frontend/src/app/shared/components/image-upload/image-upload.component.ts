dc913ca7a215121e54bd2e3d739f3b5770c4a27b
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-17 16:10:55 -0800

    feat: minor changes in getting url.

--- frontend/src/app/shared/components/image-upload/image-upload.component.ts
+++ frontend/src/app/shared/components/image-upload/image-upload.component.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireStorage } from "@angular/fire/storage";
 import { map, finalize } from "rxjs/operators";
@@ -12,29 +12,18 @@ export class ImageUploadComponent {
   downloadURL: Observable<string>;
   fb;
   constructor(private storage: AngularFireStorage) { }
+  @Output() imageUrl = new EventEmitter();
+
   onFileSelected(event) {
     var n = Date.now();
     const file = event.target.files[0];
-    const filePath = `RoomsImages/${n}`;
+    const filePath = `Images/${n}`;
     const fileRef = this.storage.ref(filePath);
-    const task = this.storage.upload(`RoomsImages/${n}`, file);
-    task
-      .snapshotChanges()
-      .pipe(
-        finalize(() => {
-          this.downloadURL = fileRef.getDownloadURL();
-          this.downloadURL.subscribe(url => {
-            if (url) {
-              this.fb = url;
-            }
-            console.log(this.fb);
-          });
-        })
-      )
-      .subscribe(url => {
-        if (url) {
-          console.log(url);
-        }
-      });
+    const task = this.storage.upload(`Images/${n}`, file);
+    task.then((ref) => {
+      ref.ref.getDownloadURL().then(url => {
+        this.imageUrl.emit(url)
+      })
+    })
   }
 }
