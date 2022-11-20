3f6727b1b7b5b88d5e0a01e2039f3085d6e8ab9d
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-19 21:15:10 -0800

    feature: endpoints added.

--- backend/src/main/java/com/crowdfunding/app/controller/PrivateEndpoint.java
+++ backend/src/main/java/com/crowdfunding/app/controller/PrivateEndpoint.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("private")
 public class PrivateEndpoint {
@@ -19,16 +21,28 @@ public class PrivateEndpoint {
         return ResponseEntity.ok(user);
     }
 
+    @RequestMapping(value = "/project-create", method = RequestMethod.POST)
+    public PROJECT createProject(@RequestBody PROJECT project){
+        return projectService.createProject(project);
+    }
+    @GetMapping("/get_all_projects")
+    public List<PROJECT> getAllProjects(){
+        return projectService.getList();
+    }
+    @GetMapping("/get_my_projects")
+    public List<PROJECT> getMyProjects(){
+        return projectService.getMyProjects();
+    }
+
+    @GetMapping("/get_project_by_slug")
+    public PROJECT getProjectBySlug(String slug){
+        return projectService.getProjectBySlug(slug);
+    }
+
     @DeleteMapping(value = "/project-delete")
     ResponseEntity<String> deleteProject(String project_uid) {
         projectService.deleteProject(project_uid);
         return ResponseEntity.ok("OK");
     }
-
-    @RequestMapping(value = "/project-create", method = RequestMethod.POST)
-    ResponseEntity<String> createProject(PROJECT project){
-        projectService.createProject(project);
-        return ResponseEntity.ok("OK"); }
-
 
 }
