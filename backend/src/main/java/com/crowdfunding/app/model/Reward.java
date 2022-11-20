515d304431a46e8a0e44b8c65843748d94f49fb0
Tony Mathen <tonymathen@Tonys-MacBook-Pro.local>
2022-11-19 16:43:49 -0800

    feat: added project crate and delete controllers

--- backend/src/main/java/com/crowdfunding/app/model/Reward.java
+++ backend/src/main/java/com/crowdfunding/app/model/Reward.java
@@ -12,36 +12,6 @@ public class Reward extends BaseModel{
     String projectId;
     String rewardedTo;
     String description;
-    public Reward(String uid, String createdAt, String updatedAt, String projectId, String rewardedTo,
-            String description) {
-        super(uid, createdAt, updatedAt);
-        this.projectId = projectId;
-        this.rewardedTo = rewardedTo;
-        this.description = description;
-    }
 
-    public String getprojectId() //getter
-    {
-        return getprojectId();
-    }
-
-    public String getrewardedto()
-    {
-        return getrewardedto();
-    }
-
-    public String getDescription()
-    {
-        return getDescription();
-    }
-
-    
-
-
-    
-   
-
-   
-   
 
 }
