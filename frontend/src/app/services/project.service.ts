73a82b5cc83e5468d37ff60d0b083e983eb1fb05
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-19 21:16:37 -0800

    feat: added project service.

---
+++ frontend/src/app/services/project.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: "root"
+})
+export class ProjectService {
+  constructor(private http: HttpClient) { }
+  create(payload) {
+    return this.http.post(environment.backend_url + "/project-create", {
+      ...payload
+    });
+  }
+  getMyProjects() {
+    return this.http.get(environment.backend_url + "/get_my_projects");
+  }
+  getProjectBySlug(slug: String) {
+    return this.http.get(`${environment.backend_url}/get_project_by_slug?slug=${slug}`);
+  }
+  getAllProjects() {
+    return this.http.get(`${environment.backend_url}/get_all_projects`);
+  }
+}
