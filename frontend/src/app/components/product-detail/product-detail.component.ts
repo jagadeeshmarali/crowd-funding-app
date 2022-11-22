810a98a3579345085303e8c6f92c25196c4bc704
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-22 14:18:17 -0800

    feat: completed the product detail functionality.

--- frontend/src/app/components/product-detail/product-detail.component.ts
+++ frontend/src/app/components/product-detail/product-detail.component.ts
@@ -1,7 +1,8 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { AuthenticationService, ProjectService } from '../../services';
+import { AuthenticationService, ProjectService, RewardService, TransactionService } from '../../services';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,33 +11,66 @@ import { AuthenticationService, ProjectService } from '../../services';
 })
 export class ProductDetailComponent implements OnInit {
   projectDetail: any;
+  rewards: any;
+  transactions: any;
 
-  constructor(private modalService: NgbModal, private projectService: ProjectService, public route: ActivatedRoute, public authService: AuthenticationService) {
+  constructor(private modalService: NgbModal,
+    private projectService: ProjectService,
+    public route: ActivatedRoute,
+    public authService: AuthenticationService,
+    public rewardService: RewardService,
+    public transactionService: TransactionService
+  ) {
     console.log(this.route.snapshot.params["slug"]);
     this.projectService.getProjectBySlug(this.route.snapshot.params["slug"]).toPromise().then((data) => {
       console.log(data);
+      this.rewardService.getRewardByProjectId(data["id"]).toPromise().then((d: Array<any>) => {
+        console.log(d)
+        this.rewards = d;
+      });
+      this.transactionService.getProjectTransactions(data["id"]).toPromise().then((transactions: Array<any>) => {
+        console.log(transactions);
+        this.transactions = transactions;
+      })
       this.projectDetail = data;
     })
 
   }
 
-  imageSrc = 'assets/images/product/1.jpg';
-
-  sizeClass = "";
-
   ngOnInit(): void {
-  }
-
-  onClick(imagename: any) {
-    this.imageSrc = "assets/images/product/" + imagename;
   }
 
   open(content: any) {
     this.modalService.open(content);
   }
+  paymentForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    cardNumber: new FormControl('', [Validators.required]),
+    expiration: new FormControl('', [Validators.required]),
+    cvv: new FormControl('', [Validators.required]),
+    amount: new FormControl("", [Validators.required])
+  });
 
-  toggleSizeClass(size: any) {
-    this.sizeClass = size;
+
+
+  selectedReward(data) {
+    console.log(data);
+    let payload = {
+      ...data,
+      projectId: this.projectDetail["id"],
+      rewardId: data["id"],
+      projectTitle: this.projectDetail["title"],
+      projectImageUrl: this.projectDetail["imageUrl"],
+      projectSlug: this.projectDetail['slug']
+    }
+    this.paymentForm.get("amount").setValue(payload["rewardAmount"])
+    this.paymentForm.get("amount").disable();
+    // delete payload['id']
+    // // console.log(payload);
+    // this.transactionService.create(payload).toPromise().then((data) => {
+    //   console.log(data);
+    // })
+
   }
 
 }
