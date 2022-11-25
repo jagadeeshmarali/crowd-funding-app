a8eeae6a302426160cb683ff4f507262226ba0a9
Abhsihek Praveenkumar Muttath <63782877+ABHISHEK22415@users.noreply.github.com>
2022-11-25 02:09:27 -0800

    Updating getter and setter

--- backend/src/main/java/com/crowdfunding/app/model/auth/Credentials.java
+++ backend/src/main/java/com/crowdfunding/app/model/auth/Credentials.java
@@ -1,11 +1,9 @@
 package com.crowdfunding.app.model.auth;
 
 import com.google.firebase.auth.FirebaseToken;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
+
+
 public class Credentials {
 
 	public enum CredentialType {
@@ -17,4 +15,46 @@ public class Credentials {
 	private String idToken;
 	private String session;
 
+	
+
+	public Credentials(CredentialType type, FirebaseToken decodedToken, String idToken, String session) {
+		this.type = type;
+		this.decodedToken = decodedToken;
+		this.idToken = idToken;
+		this.session = session;
+	}
+
+	public CredentialType getType() {
+		return type;
+	}
+
+	public void setType(CredentialType type) {
+		this.type = type;
+	}
+
+	public FirebaseToken getDecodedToken() {
+		return decodedToken;
+	}
+
+	public void setDecodedToken(FirebaseToken decodedToken) {
+		this.decodedToken = decodedToken;
+	}
+
+	public String getIdToken() {
+		return idToken;
+	}
+
+	public void setIdToken(String idToken) {
+		this.idToken = idToken;
+	}
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
 }
+
