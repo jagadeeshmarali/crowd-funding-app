a0b5498e9dffe087487a1af18ece66d82cb8de09
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-25 16:37:21 -0800

    fix: minor fix.

--- backend/src/main/java/com/crowdfunding/app/model/auth/CookieProperties.java
+++ backend/src/main/java/com/crowdfunding/app/model/auth/CookieProperties.java
@@ -1,6 +1,9 @@
 package com.crowdfunding.app.model.auth;
 
 
+import lombok.Data;
+
+@Data
 public class CookieProperties {
 	String domain;
 	String path;
@@ -9,47 +12,47 @@ public class CookieProperties {
 	int maxAgeInMinutes;
 	
 
-	public CookieProperties(String domain, String path, boolean httpOnly, boolean secure, int maxAgeInMinutes) {
-		this.domain = domain;
-		this.path = path;
-		this.httpOnly = httpOnly;
-		this.secure = secure;
-		this.maxAgeInMinutes = maxAgeInMinutes;
-	}
-
-	public String getDomain() {
-		return domain;
-	}
-	public void setDomain(String domain) {
-		this.domain = domain;
-	}
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-	public boolean isHttpOnly() {
-		return httpOnly;
-	}
-
-	public void setHttpOnly(boolean httpOnly) {
-		this.httpOnly = httpOnly;
-	}
-	public boolean isSecure() {
-		return secure;
-	}
-
-	public void setSecure(boolean secure) {
-		this.secure = secure;
-	}
-	public int getMaxAgeInMinutes() {
-		return maxAgeInMinutes;
-	}
-
-	public void setMaxAgeInMinutes(int maxAgeInMinutes) {
-		this.maxAgeInMinutes = maxAgeInMinutes;
-	}
+//	public CookieProperties(String domain, String path, boolean httpOnly, boolean secure, int maxAgeInMinutes) {
+//		this.domain = domain;
+//		this.path = path;
+//		this.httpOnly = httpOnly;
+//		this.secure = secure;
+//		this.maxAgeInMinutes = maxAgeInMinutes;
+//	}
+//
+//	public String getDomain() {
+//		return domain;
+//	}
+//	public void setDomain(String domain) {
+//		this.domain = domain;
+//	}
+//	public String getPath() {
+//		return path;
+//	}
+//
+//	public void setPath(String path) {
+//		this.path = path;
+//	}
+//	public boolean isHttpOnly() {
+//		return httpOnly;
+//	}
+//
+//	public void setHttpOnly(boolean httpOnly) {
+//		this.httpOnly = httpOnly;
+//	}
+//	public boolean isSecure() {
+//		return secure;
+//	}
+//
+//	public void setSecure(boolean secure) {
+//		this.secure = secure;
+//	}
+//	public int getMaxAgeInMinutes() {
+//		return maxAgeInMinutes;
+//	}
+//
+//	public void setMaxAgeInMinutes(int maxAgeInMinutes) {
+//		this.maxAgeInMinutes = maxAgeInMinutes;
+//	}
 
 }
